7e907edff6d5388f53955fc79d1cf876234753fa
Lina Zhao <69181886+lz707@users.noreply.github.com>
2020-11-08 23:04:35 -0800

    Update 1_two_sum.py

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -1,3 +1,4 @@
+# if each element is not necessarily unique
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         idx_nums = dict(zip(nums, range(len(nums))))
@@ -7,3 +8,9 @@ class Solution:
             if idx2 != None and i!=idx2:
                 return i, idx2
             
+# if each element is unique and return count of pairs only
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums2 = set([target - num for num in nums])
+        nums1 = set(nums)
+        return len(nums2&nums1)
